ee9e18b6b44e1c4699fb92988ff91bceb0a74076
Ofir-Cohen-1 <ofir.cohen22@gmail.com>
2022-11-30 18:23:40 +0200

    Added Header and Footer components

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,38 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./pages/Home/Home"
-import Products from "./pages/Products/Products"
-import Product from "./pages/Product/Product"
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Products from './pages/Products/Products'
+import Product from './pages/Product/Product'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home/>,
+    path: '/',
+    element: <Home />,
   },
   {
-    path: "/products/:id",
-    element: <Products/>,
+    path: '/products/:id',
+    element: <Products />,
   },
   {
-    path: "/product/:id",
-    element: <Product/>,
+    path: '/product/:id',
+    element: <Product />,
   },
-]);
+])
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <RouterProvider router={router} />
-    </div>
-  );
+    <>
+      <Header />
+      <main>
+        <h1>welcome to shoppy</h1>
+      </main>
+      <Footer />
+      <div className='App'>
+        <RouterProvider router={router} />
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
