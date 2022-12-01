44f421f3ca6316358a6f61b6a7a35712b16e777d
Ofir-Cohen-1 <ofir.cohen22@gmail.com>
2022-12-01 10:08:29 +0200

    Added Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,40 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './pages/Home/Home'
-import Products from './pages/Products/Products'
-import Product from './pages/Product/Product'
+// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import React from 'react'
+import { Container } from 'react-bootstrap'
+// import Home from './pages/Home/Home'
+// import Products from './pages/Products/Products'
+// import Product from './pages/Product/Product'
 import Header from './components/Header'
 import Footer from './components/Footer'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/products/:id',
-    element: <Products />,
-  },
-  {
-    path: '/product/:id',
-    element: <Product />,
-  },
-])
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <Home />,
+//   },
+//   {
+//     path: '/products/:id',
+//     element: <Products />,
+//   },
+//   {
+//     path: '/product/:id',
+//     element: <Product />,
+//   },
+// ])
 
 const App = () => {
   return (
     <>
       <Header />
       <main>
-        <h1>welcome to shoppy</h1>
+        <Container>
+          <h1>welcome to shoppy</h1>
+        </Container>
       </main>
       <Footer />
-      <div className='App'>
+      {/* <div className='App'>
         <RouterProvider router={router} />
-      </div>
+      </div> */}
     </>
   )
 }
