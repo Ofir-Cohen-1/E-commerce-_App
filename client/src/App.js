c75c58c1204018bf261c6ef3b3ea1751ebf22b0c
Ofir-Cohen-1 <ofir.cohen22@gmail.com>
2022-12-04 20:29:50 +0200

    Added dummy products to the HomeScreen

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,8 @@
-// import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import React from 'react'
 import { Container } from 'react-bootstrap'
-// import Home from './pages/Home/Home'
-// import Products from './pages/Products/Products'
-// import Product from './pages/Product/Product'
 import Header from './components/Header'
 import Footer from './components/Footer'
-
-// const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <Home />,
-//   },
-//   {
-//     path: '/products/:id',
-//     element: <Products />,
-//   },
-//   {
-//     path: '/product/:id',
-//     element: <Product />,
-//   },
-// ])
+import HomeScreen from './screens/HomeScreen'
 
 const App = () => {
   return (
@@ -28,13 +10,10 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>welcome to shoppy</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
-      {/* <div className='App'>
-        <RouterProvider router={router} />
-      </div> */}
     </>
   )
 }
